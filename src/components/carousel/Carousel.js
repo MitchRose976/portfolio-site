744847261c5736d3f3de0f0d2e9a8ab4b16332a2
MitchRose976 <mitch.j.rose@outlook.com>
2021-11-06 18:52:49 -0400

    Started making project carousel

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from "styled-components";
+
+function Carousel() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// Holds entire carousel
+const CarouselContainer = styled.div`
+
+`;
+
+// These are used for the left and right arrows that toggle to next image
+// Also used for the small little buttons at the bottom used to slide to specific item
+const CarouselButton = styled.button`
+
+`;
+
+// Holds the entire list of Carousel Items
+const CarouselTrack = styled.ul`
+
+`;
+
+// Each inidividual carousel item
+const CarouselSlide = styled.li`
+
+`;
+
+const CarouselNav = styled.div`
+
+`;
+
+
+
+export default Carousel
